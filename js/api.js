9d10895265284b50836241ae45f8583aacf2541b
Travis Heller <theller@satoriinteractive.com>
2015-03-04 01:55:24 -0500

    Flickr/Dabble api with Angular

---
+++ js/api.js
@@ -0,0 +1,210 @@
+{
+"projects": [
+    {
+        "id":"1",
+        "title":"Heller Recipes",
+        "description":"This web applications was developed to keep track of my dads recipes and make them easily accesible.He is now able to check each user and make a dinner based on what everybody likes or in some cases dont like.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0", 
+        "projectLink":"http://www.travismichael.net/HELLER-RECIPES",
+        "genre":"web app",
+        "thumb":"img/projects/heller-recipes/thumb.jpg",
+        "images": [
+            {"desktopImg":"img/projects/heller-recipes/desktop.png","desktopMobile":"img/projects/heller-recipes/mobile.png"},
+            {"desktopImg":"img/projects/4dmaxDesktop.png","desktopMobile":"img/projects/heller-recipes/mobile.png"},
+            {"desktopImg":"img/projects/bbdDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"2",
+        "title":"3D Animation",
+        "description":"Created using 4D Cinema Max, a 3d anitmation program that allows you to create realistic renderings and animations.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0", 
+        "projectLink":"http://www.google.com",
+        "genre":"3d",
+        "thumb":"img/projects/4dmax.jpg",
+        "images": [
+            {"desktopImg":"img/projects/4dmaxDesktop.png","desktopMobile":"img/projects/heller-recipes/mobile.png"},
+            {"desktopImg":"img/projects/4dmaxDesktop.png","desktopMobile":"img/projects/heller-recipes/mobile.png"},
+            {"desktopImg":"img/projects/4dmaxDesktop.png","desktopMobile":"img/projects/heller-recipes/mobile.png"}
+        ]
+    },
+    {
+        "id":"3",
+        "title":"Body by Design",
+        "description":"This web applications was developed to keep track of my dads recipes and make them easily accesible.He is now able to check each user and make a dinner based on what everybody likes or in some cases dont like.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0", 
+        "projectLink":"http://www.google.com",
+        "genre":"wordpress",
+        "thumb":"img/projects/bbd.jpg",
+        "images": [
+            {"desktopImg":"img/projects/bbdDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/bbdDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/bbdDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"4",
+        "title":"Team Bootcamp",
+        "description":"T-shirt Design for Lifetime Fitness",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0", 
+        "projectLink":"http://www.google.com",
+        "genre":"illustration",
+        "thumb":"img/projects/bootcamp.jpg",
+        "images": [
+            {"desktopImg":"img/projects/bootcampDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/bootcampDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/bootcampDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"5",
+        "title":"Digital Painting",
+        "description":"Digital Painting with graphic tablet",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0", 
+        "projectLink":"http://www.google.com",
+        "genre":"illustration",
+        "thumb":"img/projects/digitalpaint.jpg",
+        "images": [
+            {"desktopImg":"img/projects/digitalpaintDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/digitalpaintDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/digitalpaintDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"6",
+        "title":"Alien Flash Animation",
+        "description":"T-shirt Design for Lifetime Fitness",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"animation",
+        "thumb":"img/projects/flashalien.jpg",
+        "images": [
+            {"desktopImg":"img/projects/flashalienDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/flashalienDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"7",
+        "title":"InHealth CDS",
+        "description":"Web site made for inhealth cds.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0", 
+        "projectLink":"http://www.google.com",
+        "genre":"web design",
+        "thumb":"img/projects/inhealthcds.jpg",
+        "images": [
+            {"desktopImg":"img/projects/inhealthcdsDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/inhealthcdsDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/inhealthcdsDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/inhealthcdsDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"8",
+        "title":"InHealth Product Video",
+        "description":"T-shirt Design for Lifetime Fitness",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"web design",
+        "thumb":"img/projects/inhealthvideo.jpg",
+        "images": [
+            {"desktopImg":"img/projects/inhealthvideoDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/inhealthvideoDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"9",
+        "title":"Single Web Page App",
+        "description":"Single web page application made for marketing the sales of longrange treatment of cattle to show the difference between users company and the competitors treatment product.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"web app",
+        "thumb":"img/projects/longrange.jpg",
+        "images": [
+            {"desktopImg":"img/projects/longrangeDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/longrangeDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/longrangeDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"10",
+        "title":"Single Web Page App",
+        "description":"Single web page application made for marketing the sales of preventive treatment for dogs to show the difference between users company and the competitors treatment product.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"web app",
+        "thumb":"img/projects/nexgard.jpg",
+        "images": [
+            {"desktopImg":"img/projects/nexgardDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"11",
+        "title":"Pinup Illustration",
+        "description":"2d illustration of a war pinup girl using graphite pencils.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"illustration",
+        "thumb":"img/projects/pinup.jpg",
+        "images": [
+            {"desktopImg":"img/projects/pinupDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/pinupDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/pinupDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/pinupDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"12",
+        "title":"Responsive Web Design",
+        "description":"The ability to design web apps using mobile first concepts. A quick representation of a starbucks strore app using responsive design.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"web design",
+        "thumb":"img/projects/rwd.jpg",
+        "images": [
+            {"desktopImg":"img/projects/rwdDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/rwdDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"13",
+        "title":"Web Agency Site Developement",
+        "description":"An interactive site that displays the agencies character and style using animations and a style guide that meshes with the branding and image of the company.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"web design",
+        "thumb":"img/projects/satori.jpg",
+        "images": [
+            {"desktopImg":"img/projects/satoriDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/satoriDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/satoriDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/satoriDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"14",
+        "title":"Tshirt-Design for Fitness club",
+        "description":"Tshirt design made for a Crossfit fitness club called NoQuarter",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"illustration",
+        "thumb":"img/projects/tshirtdesign.jpg",
+        "images": [
+            {"desktopImg":"img/projects/tshirtdesignDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/tshirtdesignDesktop.png","desktopMobile":""}
+        ]
+    },
+    {
+        "id":"15",
+        "title":"Tritak Single page Applicaiton",
+        "description":"Single web page app made for Merials Tritak treatment. Was handed a brochure filled with info on tritak and was asked to turn it into a interactive peice so that their marketing team could use it as a selling peice. Was made for tablet devices specifically.",
+        "technologiesUsed":"CodeIgniter, PHP, Sequel Pro, Javascript, jQuery,HTML5, CSS3, SASS, Foundation 5.0",
+        "projectLink":"http://www.google.com",
+        "genre":"web app",
+        "thumb":"img/projects/tritak.jpg",
+        "images": [
+            {"desktopImg":"img/projects/tritakDesktop.png","desktopMobile":""},
+            {"desktopImg":"img/projects/tritak2Desktop.png","desktopMobile":""}
+        ]
+    }
+]
+}
---
+++ js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var app = angular.module('dabbble', ['dabbble.controllers', 'dabbble.filters', 'dabbble.services']);
+
+app.config(function ($routeProvider){
+	$routeProvider
+	.when("/shots/:id", {controller:"ShotsCtrl", templateUrl: "partials/shot.html" })
+	.when("/flickr", {controller:"homeCtrl", templateUrl: "partials/welcome.html" })
+	.when("/", {controller:"homeCtrl", templateUrl: "partials/welcome.html" })
+	.when("/:list", {controller:"ShotsListCtrl", templateUrl: "partials/shots_list.html" })
+	.otherwise({redirectTo: "partials/welcome.html"});
+});
